d0038c12a77ef11d5552fe0b6cf88b1b218a74ca
lauryao <lyao_c@yahoo.fr>
2025-10-01 08:35:36 -0400

    Ajout langchain_doc_retriever and Chroma

---
+++ src/react_agent/langchain_doc_retriever.py
@@ -0,0 +1,31 @@
+from langchain_chroma import Chroma
+
+
+from chromadb.utils import embedding_functions
+class LangChainChromaEmbeddings:
+    def __init__(self):
+        self.embd=embedding_functions.DefaultEmbeddingFunction()
+    def embed_query(self, query):
+        return self.embd([query])[0]
+    def embed_documents(self,docs):
+        return self.embd(docs)
+embeddings=LangChainChromaEmbeddings()    
+
+vector_store = Chroma(
+    collection_name="example_collection",
+    embedding_function=embeddings
+)
+
+from langchain_core.documents import Document
+documents=[
+    Document("LangChain invokes LLMs"),
+    Document("LangGraph runs agents")
+]
+vector_store.add_documents(documents)
+retriever=vector_store.as_retriever(search_kwargs={
+    "k":1
+})
+res_langchain=retriever.invoke("What is LangChain?")[0]
+res_langgraph=retriever.invoke("What is LangGraph?")[0]
+print(f"Expects LangChain document, got {res_langchain}")
+print(f"Expects LangGraph document, got {res_langgraph}")
